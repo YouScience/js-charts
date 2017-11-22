ddc44e21335603686f3d9b9862f8fe4505e2dce7
Devin Castro <ddcast@gmail.com>
2017-11-22 00:04:49 -0600

    Remove slice api in favor of new labels

--- charts/donut/create.js
+++ charts/donut/create.js
@@ -26,14 +26,14 @@ JSC.prototype.DonutCreate = function(data) {
   var svg = this._svg = d3.select(this._target)
     .append('svg')
     .attr({
-      width: this._config.width,
+      width: this._config.title ? this._config.width * 2.3 : this._config.width,
       height: this._config.height,
       class: 'jsc-svg-container jsc-donut',
       style: 'background-color: ' + _self._config.backgroundColor
     })
     .append('g')
     .attr({
-      transform: 'translate(' + this._config.width / 2 + ',' + this._config.height / 2 + ')'
+      transform: 'translate(' + ( this._config.title ? ( this._config.width / 2 ) * 2.3 : this._config.width / 2 ) + ',' + this._config.height / 2 + ')'
     });
 
   var path = svg.selectAll('path')
@@ -104,14 +104,8 @@ JSC.prototype.DonutCreate = function(data) {
       });
   }
 
-  var colorstore = {};
-
   path.attr('fill', function(d, i) {
-      var fill = color(i);
-
-      colorstore[i] = fill;
-
-      return fill;
+      return color(i);
     })
     .attr('class', function(d, i) {
       return 'jsc-slice jsc-slice--' + i;
@@ -122,66 +116,92 @@ JSC.prototype.DonutCreate = function(data) {
     .attr('stroke-width', function(d, i) {
       return i == 0 ? 4  : 5;
     })
-    .on(this._config.selectevent, debounce(function(d, i) {
-      this.parentElement.insertBefore(this, this.parentElement.firstChild);
+    .on(this._config.selectevent, debounce(selecthandler, 100))
+    .on(this._config.selectblurevent, debounce(selectblurhandler, 100));
 
-      svg.selectAll('.jsc-slice')
-        .attr('stroke-width', 5)
-        .attr('stroke', _self._config.backgroundColor);
+  function selecthandler(d, i) {
+    this.parentElement.insertBefore(this, this.parentElement.firstChild);
 
-      d3.select(this)
-        .attr('stroke-width', 4)
-        .attr('stroke', function() {
-          return this.getAttribute('fill');
-        })
-      
-      if (!_self._config.title) {
-        svg.selectAll('.jsc-text')
-          .attr('visibility', 'hidden');
+    svg.selectAll('.jsc-slice')
+      .attr('stroke-width', 5)
+      .attr('stroke', _self._config.backgroundColor);
 
-        svg.selectAll('.jsc-text--' + i)
-          .attr('visibility', 'visible');
-      }
+    d3.select(this)
+      .attr('stroke-width', 4)
+      .attr('stroke', function() {
+        return this.getAttribute('fill');
+      })
+    
+    if (!_self._config.title) {
+      svg.selectAll('.jsc-text')
+        .attr('visibility', 'hidden');
 
-      selectSlice(i);
+      svg.selectAll('.jsc-text--' + i)
+        .attr('visibility', 'visible');
+    }
 
-      _self._config.onselect.call(svg, findSlice(i));
-    }, 100));
+    _self._config.onselect.call(svg, d, i);
+  }
 
+  function selectblurhandler(d, i) {
+    _self._config.onselectblur.call(svg, d, i);
+  }
 
-  var labelGroup = svg.append('svg:g')
-    .attr('class', 'label-group');
+  if (this._config.title) {
+    var labels = svg.append('svg:g')
+      .attr('class', 'labels');
 
-  var sliceLabel = labelGroup.selectAll('text')
-    .data(pie(this._data));
+    var text = labels.selectAll('text')
+      .data(pie(this._data), function(d) { return d.data.name; });
 
-  var slicestore = [];
+    text.enter()
+      .append('text')
+      .attr('class', function(d, i) {
+        return 'jsc-label jsc-label--' + i;
+      })
+      .attr('dy', '.35em')
+      .text(function(d) {
+        return d.data.name;
+      })
+      .on(this._config.selectevent, debounce(function(d, i) {
+        var slice = svg.select('.jsc-slice--' + i);
 
-  sliceLabel.enter().append('svg:text')
-    .attr('class', 'arc-label')
-    .attr('transform', function(d) {return 'translate(' + arc.centroid(d) + ')'; })
-    .attr('text-anchor', 'middle')
-    .attr('fill-opacity', '0.0')
-    .text(function(d, i) {
-      var crect = this.getBoundingClientRect();
-      var prect = this.ownerSVGElement.getBoundingClientRect();
+        selecthandler.apply(slice[0][0], [d, i]);
+      }, 100))
+      .on(this._config.selectblurevent, debounce(function(d, i) {
+        var slice = svg.select('.jsc-slice--' + i);
 
-      slicestore.push({
-        index: i,
-        selected: ( 0 == i ),
-        data: data[i],
-        coords: { x: crect.x - prect.x, y: crect.y - prect.y },
-        side: {
-          left: crect.x - prect.x < _self._config.width / 2,
-          right: crect.x - prect.x >= _self._config.width / 2
-        },
-        fill: colorstore[i]
+        selectblurhandler.apply(slice[0][0], [d, i]);
+      }, 100));
+
+    text.attr('transform', function(d) {
+        var pos = arc.centroid(d);
+        pos[0] = innerRadius * (midAngle(d) < Math.PI ? 1 : -1);
+        return 'translate('+ pos +')';
+      })
+      .attr('text-anchor', function(d) {
+        return midAngle(d) < Math.PI ? 'start':'end';
       });
 
-      return '-';
-    });
+    var lines = svg.append('g')
+      .attr('class', 'lines');
 
-  sliceLabel.data(pie(this._data));
+    var polyline = lines.selectAll('polyline')
+      .data(pie(this._data), function(d) { return d.data.name; });
+    
+    polyline.enter()
+      .append('polyline');
+
+    polyline.attr('points', function(d) {
+        var pos = arc.centroid(d);
+        pos[0] = innerRadius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+        return [ arc.centroid(d), arc.centroid(d), pos ];    
+      })
+      .attr('style', 'stroke-width: 2px; fill: none;')
+      .attr('stroke', function(d, i) {
+        return color(i);
+      });
+  }
 
   function fontPx(value) {
     if ('number' != typeof value) {
@@ -190,23 +210,9 @@ JSC.prototype.DonutCreate = function(data) {
 
     return value + 'px';
   }
-
-  function selectSlice(sliceIndex) {
-    slicestore.forEach(function(slice, index) {
-      slice.selected = ( slice.index === sliceIndex );
-    });
-  }
-
-  function findSlice(sliceIndex) {
-    var result = null;
-
-    slicestore.forEach(function(slice, index) {
-      if ( slice.index === sliceIndex ) {
-        result = slice
-      }
-    });
-
-    return result;
+  
+  function midAngle(d){
+    return d.startAngle + (d.endAngle - d.startAngle) / 2;
   }
 
   return {
@@ -215,9 +221,6 @@ JSC.prototype.DonutCreate = function(data) {
     },
     data: function() {
       return data;
-    },
-    slices: function() {
-      return slicestore;
     }
   };
 };
--- charts/donut/init.js
+++ charts/donut/init.js
@@ -12,7 +12,9 @@ JSC.prototype.DonutInit = function(options) {
   // Make same as width because the chart is square.
   config.height = config.width;
   config.selectevent = options.selectevent || 'mouseenter';
+  config.selectblurevent = options.selectblurevent || 'mouseleave';
   config.onselect = options.onselect || function noop() {};
+  config.onselectblur = options.onselectblur || function noop() {};
   config.title = options.title;
 
   this._config = config;
